0aeca461ef57cfbc4181b34260dd1d807ab14346
Daniel Gama <dan.syler@gmail.com>
2026-07-13 22:48:02 -0400

    vatua2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://institutomultiplica.rio',
+  site: 'https://www.imultiplicario.com.br',
   vite: {
     plugins: [tailwindcss()]
   },
