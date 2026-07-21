7d583546074626cb1ed53736de106f8e991f9e22
gamasilva <dan.syler@gmail.com>
2026-07-21 13:39:45 -0300

    v1511

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.imultiplicario.com.br',
+  site: 'https://imultiplicario.org.br',
   vite: {
     plugins: [tailwindcss()]
   },
